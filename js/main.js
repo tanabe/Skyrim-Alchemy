f0813e308894c610224f7a00e14a43f5e3d175fa
Hideaki Tanabe <tanablog@gmail.com>
2013-02-10 22:01:47 +0900

    Add ad and fix bug.

--- js/main.js
+++ js/main.js
@@ -2,11 +2,14 @@
   if (window.addEventListener) {
     window.addEventListener('hashchange', function() {
       $(document).find('tr').css('background-color', '#272b30');
-
-      var targetName = location.hash.replace('#', '');
-      var tr = $(document).find('a[name="' + targetName + '"]').closest('tr');
-      console.log(tr);
-      tr.css('background-color', '#891100');
+      var targetName = decodeURI(location.hash.replace('#', ''));
+      var anchors = $(document).find('td.effectName a, td.ingredientName a');
+      anchors.each(function(index, element) {
+        if (element.name === targetName) {
+          var tr = $(element).closest('tr');
+          tr.css('background-color', '#891100');
+        }
+      });
       var currentScrollTop = $('body').scrollTop();
       var margin = 50;
       if (($('html').height() - currentScrollTop) - document.documentElement.clientHeight > margin ) {
